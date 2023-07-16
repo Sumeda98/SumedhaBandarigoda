2f08a1e4d2ef7c1381b7cf388830c23501c0d10f
Sumeda98 <ssbsankalpa@gmail.com>
2023-07-16 22:04:53 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import './App.css';
-import { useEffect, useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Preloader from '../src/components/Pre'
-import Home from './pages/Home.js'
-import About from './pages/About'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import Project from './pages/Project';
-import Contact from './pages/Contact';
+import "./App.css";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Preloader from "../src/components/Pre";
+import Home from "./pages/Home.js";
+import About from "./pages/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { motion } from "framer-motion";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
 
 function App() {
   const [load, upadateLoad] = useState(true);
   const [mousePosition, setMousePosition] = useState({
     x: 0,
-    y: 0
+    y: 0,
   });
   // console.log(mousePosition);
 
   useEffect(() => {
-    const mouseMove = e => {
+    const mouseMove = (e) => {
       setMousePosition({
         x: e.clientX,
-        y: e.clientY
-      })
-    }
+        y: e.clientY,
+      });
+    };
 
     window.addEventListener("mousemove", mouseMove);
 
     return () => {
       window.removeEventListener("mousemove", mouseMove);
-    }
+    };
   }, []);
 
   const variants = {
     default: {
       x: mousePosition.x - 16,
-      y: mousePosition.y - 16
-    }
-  }
+      y: mousePosition.y - 16,
+    },
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -49,19 +49,16 @@ function App() {
 
   return (
     <div className="App">
-      <motion.div
-        className="cursor"
-        variants={variants}
-        animate="default"
-      />
+      <motion.div className="cursor" variants={variants} animate="default" />
       <Router>
         <Preloader load={load} />
         <div className="App" id={load ? "no-scroll" : "scroll"}>
           <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/about' element={<About />}></Route>
-            <Route path='/project' element={<Project />}></Route>
-            <Route path='/contact' element={<Contact />}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/project" element={<Project />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/SumedhaBandarigoda" element={<Home />}></Route>
           </Routes>
         </div>
       </Router>
